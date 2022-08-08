534b533092f27055a96858ca7942bcc6ad1f391a
snehaSinha0199 <85055360+snehaSinha0199@users.noreply.github.com>
2022-08-08 14:28:44 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ArithmeticLibrary;
+using InheritLibrary;
+
+B b = new B();
+C c = new C();
+
+//Calling main() of B class
+//b.main();
+
+//Calling main() of C class
+
+//c.main();
+
+Class1 c1 = new Class1();
+Class1 c2 = new Class1();
+Class3 c3 = new Class3();
+Console.WriteLine(c1.add(10));
+Console.WriteLine(c2.add(20));
